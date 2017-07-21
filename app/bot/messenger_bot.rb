bdb64fa0b2cafa43c63d34e9efdec8caeea1080a
Y0rTz <skylarking420@gmail.com>
2017-07-22 01:39:45 +0200

    Better counting of results.

--- app/bot/messenger_bot.rb
+++ app/bot/messenger_bot.rb
@@ -142,9 +142,9 @@ def show_result postback, user
   #Get sesult from User model
   surname = user.first_name
   answers_group_count = (1..2).map {|n| Answer.results_for("answer_#{n.humanize}") } #user.get_answers
-  results = answers_group_count.each_with_index do |a, i|
-    "Domanda #{i}:"<<  answers_group_count[i].map {|k,v| "Opzione #{k}: #{v}" }.join("\s")
-  end
+  results = answers_group_count.each_with_index.map do |a, i|
+    "Domanda #{i+1}:\s" <<  answers_group_count[i].map {|k,v| "Opzione #{k} => #{v}" }.join(",\s")
+  end.join("\s")
   postback.reply(text: "Ok #{surname}, ecco qui i risultati")
   postback.reply(text: "RISULTATI: #{results}")
 end
