1c978476d6e2f22c2f6dc610490202188b6f2c79
Y0rTz <skylarking420@gmail.com>
2017-07-21 12:01:39 +0200

    Typo.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :answers
 
   def get_answers
-		return self.answers.first.attributes.slice('answer_one', 'answer_two').values
+    self.answers.first.attributes.slice('answer_one', 'answer_two').values
   end
 
 end
